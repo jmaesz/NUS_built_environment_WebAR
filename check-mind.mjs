a4d442251136a78de77a64459fe75c712a753268
jmaesz <e1398236@u.nus.edu>
2026-05-24 01:40:00 +0800

    Clean up repo: remove build scripts, unused models, dead code

--- check-mind.mjs
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs';
-import { decode } from '@msgpack/msgpack';
-
-const f = decode(readFileSync('targets (25).mind'));
-const item = f.dataList[0];
-console.log('targets:', f.dataList.length);
-console.log('img:', item.targetImage.width, 'x', item.targetImage.height);
-console.log('matchingData keys:', Object.keys(item.matchingData).length);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Camera from './pages/Camera'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/camera" element={<Camera />} />
     </Routes>
   )
 }
